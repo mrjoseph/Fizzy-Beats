29c76c66230b070668b3de7e1e8938c36ede1136
Trevor Joseph <trevor.joseph@johnlewis.co.uk>
2018-10-20 19:55:11 +0100

    configured webpack to generate html file for dev

--- webpack.config.js
+++ webpack.config.js
@@ -3,49 +3,55 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 const devServerHTML = () => {
-  return new HtmlWebPackPlugin({
-    template: "./template/index.html",
-    filename: "./index.html"
-  });
+    return new HtmlWebPackPlugin({
+      template: "./webroot/template/index.html",
+      filename: "./index.html"
+    });
 }
-module.exports = {
-  entry: './webroot/client/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'js/bundle.js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      }
-    ]
-  },
-  plugins: [
-    devServerHTML(),
+module.exports = (env, argv) => {
+  const { mode } = argv;
+  const plugins = [
     new MiniCssExtractPlugin({
       filename: "css/[name].css",
       chunkFilename: "[id].css"
     })
-  ]
-};
+]
+  if (mode === 'development') {
+    plugins.push(devServerHTML())
+  }
+  return {
+    entry: './webroot/client/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'js/bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: true }
+            }
+          ]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"]
+        }
+      ]
+    },
+    plugins
+  };
+}
