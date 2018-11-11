9014080858d1a2b9842a57a2fe3993f5427889fe
Trevor Joseph <trevor.joseph@johnlewis.co.uk>
2018-11-11 13:29:24 +0000

    fix webpack dev server to now render out our graphql

--- webroot/server/server.js
+++ webroot/server/server.js
@@ -4,6 +4,7 @@ import  mongoose from 'mongoose';
 import graphqlHTTP from 'express-graphql';
 import renderHTML from './routes/index';
 import schema from './graphql-schema/user/index';
+import cors from 'cors';
 
 const uri = `mongodb://127.0.0.1:27017/mixdown`;
 
@@ -14,6 +15,7 @@ mongoose.connection.once('open', () => {
 });
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
@@ -23,7 +25,8 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 app.use('/', async (req, res) => {
-  res.send(await renderHTML(req, res));
+  const html = await renderHTML(req, res)
+  res.send(html);
 });
 
 const PORT = 5000;
@@ -32,14 +35,13 @@ app.listen(PORT, () => {
 });
 
 /* App set up
-* TODO add 404 and 500 pages
+* TODO and 500 pages
+* TODO add environment veriables
 * TODO add Redux to handle app state server side
 * TODO add redux logger for development build
-* TODO Set-up MongoDB and Mongoose models
 * TODO set up social media accounts, TW, YouTube, SC, BandCamp
 * TODO How do we render content in the header? React helmet
 * TODO add pre commit hook
-* todo graphql
 * TODO Build assets
 * TODO SASS / Base css / css modules
 * TODO FAVICON
