6dd4b7183f95348e697601f25e12bcd3aa0b137a
Trevor Joseph <trevor.jospeh@thetrainline.com>
2018-12-11 05:41:05 +0000

    added media queries to dropdown

--- webroot/client/components/header-hav/HeaderNav.styles.js
+++ webroot/client/components/header-hav/HeaderNav.styles.js
@@ -3,11 +3,31 @@ import styled from 'styled-components';
 export const HeaderNavContainer = styled.div`   
   transition:max-height .5s ease-in-out;
   overflow:hidden;
-  &.showContent {
+    &.showContent {
     max-height:600px;
      height:auto;
   }
   &.hideContent {
-   max-height:0; 
+     max-height:0; 
+       @media only screen and (min-width: 991px) {
+        max-height:600px;
+        
+    }
   }
+  
+  @media only screen and (min-width: 991px) {
+  transition:max-height .5s ease-in-out;
+      max-height:600px;
+     height:auto;
+  
+}
+`;
+
+
+const foo = `
+  &.showContent {
+    max-height:600px;
+     height:auto;
+  }
+ 
 `;
